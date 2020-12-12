9130e8c8f4c1dc16afe2c5891c4759a90cbe90bd
Ahmad Zakaria Fathoni <60414357+azarafath@users.noreply.github.com>
2020-12-12 18:28:27 +0700

    Update Covid-19.py
    Fixed some problem and add if statments

--- Covid-19.py
+++ Covid-19.py
@@ -19,7 +19,6 @@ print("NIM  : 3.34.20.4.01")
 print("")
 print("")
 while True:
-    if pilih == 1:
       for x in soal:
         print(x)
         tanya = input("Apakah iya atau tidak ? (Y/T):")
@@ -30,34 +29,52 @@ while True:
           print("")
           nilai.append(0)
 
-    cls()
-    print("HASIL TEST ONLINE")
-    if(nilai[0] == 1 ):
-      print("")
-      print("Kamu termasuk kasus POSITIF TANPA GEJALA.")
-      print("Disarankan untuk melakukan test PCR untuk memastikan kondisi kamu, dan lakukan isolasi mandiri")
-    elif(nilai[0,1] == 1):
-      print("")
-      print("Kamu termasuk kategori KASUS POSITIF DENGAN GEJALA.")
-      print("Disarankan untuk melakukan test PCR untuk memastikan kondisi kamu, dan lakukan isolasi mandiri")
-    elif(nilai[1] == 1):
-      print("")
-      print("Kamu termasuk kategori RESIKO RENDAH.")
-      print("Meski berisiko rendah, namun jangan lengah, tetap patuhi protokol kesehatan.")
-    elif(nilai[2] == 1):
-      print("")
-      print("Kamu termasuk kategori KONTAK ERAT.")
-      print("Jika kamu sudah selesai melakukan karantina mandiri selama 14 hari dan tidak ada gejala maka kamu bisa dimasukan ke dalam kriteria DISCARDED, disarankan melakukan test PCR untuk memastikan")
+      cls()
+      sum(nilai)
+      print("HASIL TEST")
+      if(nilai[0] == 1 or  nilai[0] & nilai[2] & nilai[3] == 1 ):
+        print("")
+        print("Kamu termasuk kasus POSITIF TANPA GEJALA")
+        print("Disarankan untuk melakukan test PCR untuk memastikan kondisi kamu, dan lakukan isolasi mandiri")
+      elif(nilai[1] == 1):
+        print("")
+        print("Kamu termasuk kategori RESIKO RENDAH")
+        print("Meski berisiko rendah, namun jangan lengah, tetap patuhi protokol kesehatan.")
+      elif(nilai[2] or nilai[2] & nilai[3] == 1):
+        print("")
+        print("Kamu termasuk kategori KONTAK ERAT")
+        print("Jika kamu sudah selesai melakukan karantina mandiri selama 14 hari dan tidak ada gejala maka kamu bisa dimasukan ke dalam kriteria DISCARDED, disarankan melakukan test PCR untuk memastikan")
+      elif(nilai[0] & nilai[1] & nilai[2] == 1 ):
+        print("")
+        print("Kamu termasuk kasus POSITIF TANPA GEJALA")
+        print("Disarankan untuk melakukan test PCR untuk memastikan kondisi kamu, dan lakukan isolasi mandiri")
+      elif(nilai[3] == 1):
+        print("")
+        print("Kamu termasuk kriteria Pelaku Perjalanan")
+        print("Jika kamu sudah selesai melakukan karantina mandiri selama 14 hari dan tidak ada gejala maka kamu bisa dimasukan ke dalam kriteria DISCARDED, disarankan melakukan test PCR untuk memastikan")
+      elif(nilai[0] & nilai[1]== 1 or sum(nilai) == 4):
+        print("")
+        print("Kamu termasuk kategori KASUS POSITIF DENGAN GEJALA")
+        print("Disarankan untuk melakukan test PCR untuk memastikan kondisi kamu, dan lakukan isolasi mandiri")
+      elif(nilai[1] & nilai[2] == 1 or nilai[1] & nilai[2] & nilai[3] == 1 ):
+        print("")
+        print("Kamu termasuk kasus SUSPEK atau PROBABLE")
+        print("Jika gejala kamu berat dan sesak nafas hebat maka kamu termasuk kasus PROBABLE, jika tidak maka kemungkinan masuk kriteria SUSPEK")
+      else:
+        print("")
+        print("Kamu termasuk kategori RESIKO RENDAH")
+        print("Meski berisiko rendah, namun jangan lengah, tetap patuhi protokol kesehatan.")
 
 
-  
-    ulang =""
+      ulang =""
     
-    while ulang !="Y" and ulang!= "T" :
-      cls()
-      ulang = input("Mulai lagi dari awal? [Y/T] ? ")
+      while ulang !="Y" and ulang!= "T" :
+        print("")
+        ulang = input("Mulai lagi dari awal? [Y/T] ? ")
+        cls()
         
-    if ulang == "T":
-      cls()
-      print("Aplikasi Berhenti. Exit...")
-      break
+      if ulang == "T":
+        cls()
+        print("")
+        print("Aplikasi Berhenti. Exit...")
+        break
